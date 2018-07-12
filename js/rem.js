067149c1dfc1b6745bae810f7e7fd213663e9404
zhangxaing2 <13031161152@163.com>
2018-07-12 15:31:30 +0800

    first commit

---
+++ js/rem.js
@@ -0,0 +1,14 @@
+//获取屏幕宽度
+
+let htmlWidth=document.documentElement.clientWidth || document.body.clientWidth;
+
+//获取html的dom
+
+let htmlDom=document.getElementsByTagName('html')[0];
+
+
+//设置html的fontsize
+
+htmlDom.style.fontSize=htmlWidth/10 +'px';
+
+console.log(htmlDom)
